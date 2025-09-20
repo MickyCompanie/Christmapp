c92973dba3d52024c2e249018343cb33f69ecbfb
MickyCompanie <micky.companie@gmail.com>
2025-09-20 15:23:59 +0200

    front / needed component

---
+++ frontend/src/components/Badge.vue
@@ -0,0 +1,17 @@
+<template>
+    <span :class="`*:bg-${color}-100 text-${color}-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-${color}-900 dark:text-${color}-300`">{{ title }}</span>
+</template>
+
+<script setup>
+    defineProps({
+        title: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            required: false,
+            default: "blue"
+        }
+    });
+</script>
