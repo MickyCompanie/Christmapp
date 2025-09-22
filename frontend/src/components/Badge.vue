922909d48ca37cb959ec0eee7b722e28e0c221ab
MickyCompanie <micky.companie@gmail.com>
2025-09-22 22:02:36 +0200

    front / fix badge

--- frontend/src/components/Badge.vue
+++ frontend/src/components/Badge.vue
@@ -1,9 +1,10 @@
 <template>
-    <span :class="`*:bg-${color}-100 text-${color}-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-${color}-900 dark:text-${color}-300`">{{ title }}</span>
+    <span :class="badgeClass">{{ title }}</span>
 </template>
 
 <script setup>
-    defineProps({
+
+let props = defineProps({
         title: {
             type: String,
             required: true,
@@ -14,4 +15,14 @@
             default: "blue"
         }
     });
+
+    const colorMap = {
+        blue: 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300',
+        red: 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300',
+        green: 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300',
+        yellow: 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300',
+    };
+
+    const badgeClass = `${colorMap[props.color] || colorMap.blue} text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm`;
+
 </script>
